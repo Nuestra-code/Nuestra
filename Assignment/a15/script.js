77247963dcb0103d7105508839ca447e31ddf10f
Nuestra-code <nuestram21@gmail.com>
2022-11-17 13:56:28 +0800

    JS

---
+++ Assignment/a15/script.js
@@ -0,0 +1,21 @@
+console.log(`connected`);
+
+// ESSAY
+// 1. Comparison operators are used in logical statements to determine equality or difference between variables or values. While, Assignment Operator assigns a value to a variable.
+// 2. Boolean is a data type, a form of data with only two possible values. Boolean represents one of two values: true or false.
+// 3. The Addition Assignment Operator (+=) adds a value to a variable. While, The Addition Operator (+) adds numbers.
+
+// CODE PART
+
+let x = 10;
+let y = 20;
+
+    console.log(x += y); //new value of x is 15
+    console.log(y -= x); //new value of y is -10
+    console.log(x *= y); //new value of x is -300
+    console.log(y /= x); //new value of y is 0.03333333333333333
+    console.log(x);
+    console.log(y);
+    console.log(x <= y); //true
+    console.log(x >=y); // false
+    console.log(x != y); //true
