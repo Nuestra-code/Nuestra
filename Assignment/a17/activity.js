4bf74983968bd779699ba334a33c6869f4684052
Nuestra-code <nuestram21@gmail.com>
2022-12-02 20:12:46 +0800

    Node JS Routing

---
+++ Assignment/a17/activity.js
@@ -0,0 +1,29 @@
+// What directive is used by Node.js in loading the modules it need?
+/*
+    require directive
+*/
+
+// What Node.js module contains a method for server creation?
+/*
+    http module
+*/
+
+// What is the method of the http object responsible for creating a server using Node.js
+/*
+    http.creatServer
+*/
+
+// What is the method of the response object allows us to set status codes and content types?
+/*
+    response.writeHead
+*/
+
+// Where will console.log() output its contents when run in Node.js?
+/*
+    
+*/
+
+// What property of the request object contains the address's endpoint?
+/*
+    request.url
+*/
